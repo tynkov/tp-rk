32966e5491140ea07d97748f5f74163ccd587a56
tynkov <andrey.tynkov@gmail.com>
2025-05-27 07:28:16 -0400

    edited tests

---
+++ tests/test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "ConcreteHandlerA.h"
+#include "ConcreteHandlerB.h"
+
+TEST(HandlerTest, ADelegatesToB) {
+    ConcreteHandlerA handlerA;
+    ConcreteHandlerB handlerB;
+    handlerA.setHandler(&handlerB);
+
+    ::testing::internal::CaptureStdout();
+    handlerA.handleRequest();
+    std::string output = ::testing::internal::GetCapturedStdout();
+
+    EXPECT_NE(output.find("Handled by B"), std::string::npos);
+}
+
+TEST(HandlerTest, BHandlesItself) {
+    ConcreteHandlerB handlerB;
+
+    ::testing::internal::CaptureStdout();
+    handlerB.handleRequest();
+    std::string output = ::testing::internal::GetCapturedStdout();
+
+    EXPECT_NE(output.find("Handled by B"), std::string::npos);
+}
