42b2a951661c33822cd63f6891688e9ccb2ae6b3
tynkov <andrey.tynkov@gmail.com>
2025-05-27 08:40:36 -0400

    added release building

---
+++ CPackConfig.cmake
@@ -0,0 +1,11 @@
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_PACKAGE_CONTACT "andrey.tynkov@gmail.com")
+set(CPACK_PACKAGE_VERSION "0.1.0.1")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Chain Of Responsibility C++ Design Pattern")
+
+set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
+
+set(CPACK_DEBIAN_PACKAGE_NAME "chainofresp-dev")
+set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.10")
+set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
---
+++ README.md
@@ -0,0 +1,3 @@
+##Chain Of Responsibility Design Pattern C++
+
+A chain of operations that is performed step by step if the previous one can't handle it.
